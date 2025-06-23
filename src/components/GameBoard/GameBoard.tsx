2b464980be2f6b855bd65c45004bbe2459fd3c9f
evgen-web-dev <217228725+evgen-web-dev@users.noreply.github.com>
2025-06-24 00:57:09 +0300

    refactor: add <GameField />
    add <GameField /> to render game-field cells

--- src/components/GameBoard/GameBoard.tsx
+++ src/components/GameBoard/GameBoard.tsx
@@ -3,6 +3,7 @@ import { useGameDispatch, useGameState } from "../../GameProvider"
 import type { FieldCell } from "../../types/types";
 import GameResult from "../GameResult/GameResult";
 import { GameScore } from "../GameScore/GameScore";
+import { GameField } from "../GameField/GameField";
 
 
 type GameBoardProps = {
@@ -230,12 +231,6 @@ export default function GameBoard({ }: GameBoardProps) {
 
 
 
-    function handleCellOnClick(index: number, innerIndex: number) {
-        if (!gameField.isFreezed) makeMove(index, innerIndex);
-    }
-
-
-
     function handleResetOnClick() {
         if (!gameField.isFreezed) resetGameField();
     }
@@ -257,31 +252,7 @@ export default function GameBoard({ }: GameBoardProps) {
 
                 <button onClick={handleResetOnClick} className='mb-5 !text-xs' type='button'>Reset game-field</button>
 
-                <div id="board" className="grid grid-cols-3 gap-2 md:gap-3 min-[1600px]:gap-4 aspect-square">
-                    {gameField && gameField.cells.map((curCellRow: FieldCell[], index: number) => (
-                        curCellRow.map((curCell: FieldCell, indexInner: number) =>
-                        (
-                            <button onClick={() => handleCellOnClick(index, indexInner)} key={index + '-' + indexInner}
-                                className="relative overflow-hidden lowercase rounded-lg flex items-center justify-center !text-4xl font-semibold !p-0">
-                                <span className={(curCell.isHightlighted ? (wonCellsColor + ' ') : '') + " duration-300 absolute flex items-center justify-center translate-x-[-50%] translate-y-[-50%] top-[50%] left-[50%] w-full h-full"}>
-
-                                    <svg className={(curCell.value === 'x' ? 'block' : 'hidden') + " w-[30%] h-[30%] min-[380px]:w-[35%] min-[380px]:h-[35%] stroke-black dark:stroke-white animate-svg"}
-                                        width="34" height="34" viewBox="0 0 34 34" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                        <path style={{ '--strokeDashoffsetStart': '41', 'strokeDasharray': '41' } as React.CSSProperties} d="M3 31.2843L31.2843 3.00003" strokeWidth="5" strokeLinecap="round" />
-                                        <path style={{ '--strokeDashoffsetStart': '41', 'strokeDasharray': '41' } as React.CSSProperties} d="M3 3L31.2843 31.2843" strokeWidth="5" strokeLinecap="round" />
-                                    </svg>
-
-                                    <svg className={(curCell.value === 'o' ? 'block' : 'hidden') + " w-[30%] h-[30%] min-[380px]:w-[35%] min-[380px]:h-[35%] stroke-black dark:stroke-white animate-svg"}
-                                        width="37" height="37" viewBox="0 0 37 37" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                        <path style={{ '--strokeDashoffsetStart': '101', 'strokeDasharray': '101' } as React.CSSProperties} d="M18.5 2.5C27.3366 2.5 34.5 9.66344 34.5 18.5C34.5 27.3366 27.3366 34.5 18.5 34.5C9.66344 34.5 2.5 27.3366 2.5 18.5C2.5 9.66344 9.66344 2.5 18.5 2.5Z" strokeWidth="5" />
-                                    </svg>
-
-                                </span>
-                            </button>
-                        )
-                        )
-                    ))}
-                </div>
+                <GameField onMove={makeMove} />
             </div>
 
         </>
